949ae6ce64b2bc87460cd81dda49c9bc5c1fecd3
Bogdan <bogdanguk@gmail.com>
2019-08-22 10:42:05 +0300

    created static UITableView and UITableController for show detail of product

---
+++ AppleProducts/Controllers/ProductDetailTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProductDetailTableViewController.h
+//  AppleProducts
+//
+//  Created by Bogdan Lviv on 8/22/19.
+//  Copyright © 2019 Bogdan Lviv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProductDetailTableViewController : UITableViewController <UITextFieldDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AppleProducts/Controllers/ProductDetailTableViewController.m
@@ -0,0 +1,60 @@
+//
+//  ProductDetailTableViewController.m
+//  AppleProducts
+//
+//  Created by Bogdan Lviv on 8/22/19.
+//  Copyright © 2019 Bogdan Lviv. All rights reserved.
+//
+
+#import "ProductDetailTableViewController.h"
+#import "../Model/Product.h"
+#import "../Model/ProductLine.h"
+
+@interface ProductDetailTableViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *productImageView;
+@property (weak, nonatomic) IBOutlet UITextField *productTitleTextField;
+@property (weak, nonatomic) IBOutlet UITextView *productDescriptionTextView;
+
+@property (nonatomic, strong) Product *product;
+
+@end
+
+@implementation ProductDetailTableViewController
+
+- (Product *)product{
+    if(!_product){
+        _product = [[[[ProductLine getProductLines] firstObject] products] firstObject];
+    }
+    return _product;
+}
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    self.title = @"Edit Product";
+    
+    self.productImageView.image = self.product.image;
+    self.productTitleTextField.text=self.product.title;
+    self.productTitleTextField.delegate=self;
+    self.productDescriptionTextView.text=self.product.descriptionProduct;
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(indexPath.section == 0 && indexPath.row == 0){
+        return indexPath;
+    }
+    else{
+        return nil;
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder]; //dismiss keyboard
+    return true;
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+    [self.productDescriptionTextView resignFirstResponder];
+    [self.productTitleTextField resignFirstResponder];
+}
+
+@end
